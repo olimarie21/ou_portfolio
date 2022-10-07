c0c7fea80dd77a40b81f0d3d985fac5b14e6722c
olimarie21 <olivia.underdah@gmail.com>
2022-10-06 19:21:37 -0700

    Adds material icon dependencies

--- pages/_document.js
+++ pages/_document.js
@@ -41,6 +41,10 @@ class MyDocument extends Document {
 						href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap'
 						rel='stylesheet'
 					/>
+					<link
+						rel='stylesheet'
+						href='https://fonts.googleapis.com/icon?family=Material+Icons'
+					/>
 				</Head>
 				<body>
 					<Main />
